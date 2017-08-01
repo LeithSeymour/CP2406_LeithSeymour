c40188b1336d0b45c22a290355c760b121ce3ea8
Leith Seymour <leith.seymour@my.jcu.edu.au>
2017-08-01 18:09:14 +1000

    Completed additional exercises required

---
+++ src/Prac1/MovieQuoteInfo.java
@@ -0,0 +1,14 @@
+package Prac1;
+
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+        System.out.println("This is what happens when an unstoppable force meets an immovable object. \n" +
+                "You truly are incorruptible, aren't you? You won't kill me out of some misplaced sense of \n" +
+                "self-righteousness. And I won't kill you because you're just too much fun. I think you and I are \n" +
+                "destined to do this forever.");
+        System.out.println("Movie: The Dark knight");
+        System.out.println("By: The Joker");
+        System.out.println("2008");
+
+    }
+}
