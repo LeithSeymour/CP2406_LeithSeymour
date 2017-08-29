7ce3708a47875d895acc745d29932b4280c637aa
Leith Seymour <leith.seymour@my.jcu.edu.au>
2017-08-29 20:18:07 +1000

    Finished AscendingDescending

--- src/AscendingDescending.java
+++ src/AscendingDescending.java
@@ -1,10 +1,11 @@
 import javax.swing.*;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class AscendingDescending {
     public static void main(String[] args) {
         int one, two, three, four;
-        String str, output;
+        String str;
 
         str = JOptionPane.showInputDialog(null, "Enter an integer");
         one = Integer.parseInt(str);
@@ -15,14 +16,19 @@ public class AscendingDescending {
         str = JOptionPane.showInputDialog(null, "Enter an integer");
         four = Integer.parseInt(str);
 
-        int[] inputArray = {one, two, three, four};
+        Integer[] inputArray = {one, two, three, four};
 
         System.out.println(inputArray[0]);
         System.out.println(inputArray[1]);
         System.out.println(inputArray[2]);
         System.out.println(inputArray[3]);
 
+        System.out.println("Ascending Order");
         Arrays.sort(inputArray);
+        System.out.print(Arrays.toString(inputArray));
+
+        System.out.println("\nDescending Order");
+        Arrays.sort(inputArray, Collections.reverseOrder());
         System.out.print(Arrays.toString(inputArray));
     }
 }
