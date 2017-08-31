ab9649c9022430d2673b9a62d6ca96a80242fccc
Leith Seymour <leith.seymour@my.jcu.edu.au>
2017-08-31 15:18:08 +1000

    done arrayMethoddemo

--- src/ArrayMethodDemo.java
+++ src/ArrayMethodDemo.java
@@ -1,29 +1,58 @@
-import com.sun.org.apache.bcel.internal.generic.NEW;
-
-import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.Collections;
 
 public class ArrayMethodDemo {
     public static void main(String[] args) {
-        int SLength;
         int TenInts[] = {4, 20, 3, 5, 7, 3, 2, 21, 16, 13};
-        SLength = TenInts.length;
-
         DisplayInts(TenInts);
         DisplayReversed(TenInts);
+        DisplaySum(TenInts);
+        DisplayLessThan(TenInts);
+        DisplayHigherThanCalcAvg(TenInts);
+    }
+
+    private static void DisplayHigherThanCalcAvg(int[] input) {
+        int sum = 0;
+        for (int anInput : input) {
+            sum += anInput;
+        }
+        int average = sum / input.length;
+        int IndexCounter = 0;
+        int[] NewArray = new int[input.length];
+        for (int anInput : input) {
+            if (anInput > average)
+                NewArray[IndexCounter] = anInput;
+            IndexCounter++;
+        }
+        System.out.println("Values Higher than " + average + " are\n" + Arrays.toString(NewArray));
+    }
+
+    private static void DisplayLessThan(int[] input) {
+        int i;
+        int Limiter = 15;
+        int IndexCounter = 0;
+        int[] NewArray = new int[input.length];
+        for (i = 0; i < input.length; i++) {
+            if (input[i] < Limiter)
+                NewArray[IndexCounter] = input[i];
+            IndexCounter++;
+        }
+        System.out.println("Values less than " + Limiter + " are\n" + Arrays.toString(NewArray));
+    }
+
+    private static void DisplaySum(int[] input) {
+        int sum = 0;
+        for (int anInput : input) {
+            sum += anInput;
+        }
+        System.out.println("Sum of String is \n" + sum);
     }
 
     private static void DisplayReversed(int[] input) {
-        int i;
-        int[] NewArray = {};
-
+        int[] NewArray = new int[input.length];
         System.out.println("Reversed String");
-        for (i = 0; i < input.length; ++i) {
-            NewArray[i] = input[(input.length - 1) - i]
-            ;
+        for (int i = 0; i < input.length; i++) {
+            NewArray[i] = input[(input.length - 1) - i];
         }
-
         System.out.println(Arrays.toString(NewArray));
     }
 
---
+++ src/Holder.java
@@ -0,0 +1,20 @@
+//public class Holder {
+//
+//
+//    int sum = 0;
+//        for (int anInput : input) {
+//        sum += anInput;
+//    }
+//    int average = sum / input.length;
+//    int IndexCounter = 0;
+//    int[] NewArray = new int[input.length];
+//        for (int anInput : input) {
+//        if (anInput > average)
+//            NewArray[IndexCounter] = anInput;
+//        IndexCounter++;
+//    }
+//        System.out.println("Values Higher than " + average + " are\n" + Arrays.toString(NewArray));
+//}
+//
+//
+//}
